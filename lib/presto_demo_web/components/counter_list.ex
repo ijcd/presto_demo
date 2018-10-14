d25767ee67cd353e34992b3b2bbc3bc7817148bf
Ian Duggan <ian@ianduggan.net>
2018-10-14 03:06:52 -0700

    Nested components (moved to component reference -> render)

---
+++ lib/presto_demo_web/components/counter_list.ex
@@ -0,0 +1,36 @@
+defmodule PrestoDemoWeb.Components.CounterList do
+  use Presto.Component
+  use Phoenix.HTML
+  require Logger
+
+  @impl Presto.Component
+  def initial_model(_model) do
+    []
+  end
+
+  @impl Presto.Component
+  def update(message, model) do
+    case message do
+      %{"type" => "click", "id" => "add"} ->
+        model ++ [Presto.component(PrestoDemoWeb.Components.SingleCounter)]
+
+      %{"type" => "click", "id" => "remove"} ->
+        model |> Enum.drop(-1)
+
+      _ -> model
+    end
+  end
+
+  @impl Presto.Component
+  def render(model) do
+    ~E"""
+    <div>
+      <%= for c <- model do %>
+      <%= Presto.render(c) %>
+      <% end %>
+      <button id="add" class="presto-click">Add</button>
+      <button id="remove" class="presto-click">Remove</button>
+    </div>
+    """
+  end
+end
